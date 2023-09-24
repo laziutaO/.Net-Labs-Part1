f44a6a1431c57a6387a0e4590e89f394febcf211
Oleksii <laziutaoleksij@gmail.com>
2023-09-24 13:43:36 +0300

    Added zvit

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,13 +1,11 @@
 ﻿
 using Lab1;
 
-Console.WriteLine("sdD");
 CustomQueue<int> cqueue = new CustomQueue<int>(new List<int>(){1, 2, 3, 4});
-Queue<string> queue = new Queue<string>(new List<string>(){"asdfas123123d", "asdfasf"});
-var a = cqueue.ToArray();
-string result;
-//var arr = new int[] { 11, 10, 10, 12, 15, 6 };
-//cqueue.CopyTo(arr, 0);
+int[] arr = new int[] {11, 12, 13, 14, 15, 16, 17};
+cqueue.CopyTo(arr, 1);
+foreach (var el in arr)
+    Console.WriteLine(el);
 
 
 
@@ -15,3 +13,31 @@ string result;
 
 
 
+
+
+
+/*cqueue.Enqueue(5);
+foreach(var element in cqueue)
+    Console.WriteLine(element);*/
+    
+/*var poped_element = cqueue.Dequeue();
+Console.WriteLine($"Dequeued element is: {poped_element}");
+Console.WriteLine("Collection elements are:");
+foreach(var element in cqueue)
+    Console.WriteLine(element);*/   
+    
+/*cqueue.Clear();
+foreach(var element in cqueue)
+    Console.WriteLine(element);*/
+    
+/*var hasThree = cqueue.Contains(3);
+var hasTen = cqueue.Contains(10);
+Console.WriteLine($"has 3: {hasThree}, has 10: {hasTen}");*/
+
+/*int[] arr = cqueue.ToArray();
+foreach (var el in arr)
+    Console.WriteLine(el);*/
+
+/*cqueue = cqueue.Reverse();
+foreach (var el in cqueue)
+    Console.WriteLine(el);*/
