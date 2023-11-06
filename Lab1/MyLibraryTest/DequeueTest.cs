66706b5b217a14cabec804030e9cdd81c92ad3b3
Oleksii <laziutaoleksij@gmail.com>
2023-11-06 21:43:29 +0200

    Added Enqueue and Dequeue tests

---
+++ Lab1/MyLibraryTest/DequeueTest.cs
@@ -0,0 +1,32 @@
+﻿using MyLibrary;
+namespace MyLibraryTest;
+
+public class DequeueTest
+{
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Dequeue_IntElement_SuccessfulDequeue(int expected, int[] enqueueElements)
+    {
+        CustomQueue<int> cq = new(enqueueElements);
+
+        var actualElement = cq.Dequeue();
+        
+        Assert.Equal(expected, actualElement);
+        Assert.DoesNotContain(expected, cq);
+    }
+    
+    [Fact]
+    public void Dequeue_EmptyCollection_InvalidOperationException()
+    {
+        CustomQueue<int> cq = new();
+
+        Assert.Throws<InvalidOperationException>(() => cq.Dequeue());
+    }
+    
+    public static IEnumerable<object[]> TestData()
+    {
+        yield return new object[] { 2 , new int[] { 2, 4, 5, 5, 6 } };
+        yield return new object[] { 132, new int[] { 132, 441, 5412, -55, 612 } };
+        yield return new object[] { -652, new int[] { -652, 524, 551, -7545, 452366 } };
+    }
+}
---
+++ Lab1/MyLibraryTest/EnqueueTest.cs
@@ -0,0 +1,26 @@
+﻿using MyLibrary;
+namespace MyLibraryTest;
+
+public class EnqueueTest
+{
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Enqueue_IntCollection_SuccessfulFilling(CustomQueue<int> expected, int[] enqueueElements)
+    {
+        CustomQueue<int> cq = new();
+
+        foreach (var element in enqueueElements)
+        {
+            cq.Enqueue(element);
+        }
+        
+        Assert.Equal(expected, cq);
+    }
+    
+    public static IEnumerable<object[]> TestData()
+    {
+        yield return new object[] { new CustomQueue<int>(new int[] { 2, 4, 5, 5, 6 }), new int[] { 2, 4, 5, 5, 6 } };
+        yield return new object[] { new CustomQueue<int>(new int[] { 132, 441, 5412, 55, 612 }), new int[] { 132, 441, 5412, 55, 612 } };
+        yield return new object[] { new CustomQueue<int>(new int[] { 652, 524, 551, 7545, 452366 }), new int[] { 652, 524, 551, 7545, 452366 } };
+    }
+}
