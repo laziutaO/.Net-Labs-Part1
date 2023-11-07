16647714003bd5aeed873608f641730feadd45ad
Oleksii <laziutaoleksij@gmail.com>
2023-11-07 13:51:38 +0200

    Added ToArray and Reverse tests

---
+++ Lab1/MyLibraryTest/ReverseTest.cs
@@ -0,0 +1,40 @@
+﻿using MyLibrary;
+namespace MyLibraryTest;
+
+public class ReverseTest
+{
+    [Theory]
+    [MemberData(nameof(ReverseTestData))]
+    public void Reverse_FromIntQueue_SuccessfulReverse(CustomQueue<int> expectedCq, CustomQueue<int> startingCq)
+    {
+        CustomQueue<int> cq = startingCq;
+
+        var reversedCq = cq.Reverse();
+        
+        Assert.Equal(expectedCq, reversedCq);
+    }
+
+    public static IEnumerable<object[]> ReverseTestData()
+    {
+        yield return new object[] 
+        { 
+            new CustomQueue<int>(new int[] { 73, 6, 51, 1234, -1234, 516, 53, 23 }),
+            new CustomQueue<int>(new int[] { 23, 53, 516, -1234, 1234, 51, 6, 73 }) 
+        };
+        yield return new object[] 
+        { 
+            new CustomQueue<int>(new int[] { 341, -2, -2, 51 }),
+            new CustomQueue<int>(new int[] { 51, -2, -2, 341}) 
+        };
+        yield return new object[]
+        {
+            new CustomQueue<int>(new int[] { -1 }),
+            new CustomQueue<int>(new int[] { -1 })
+        };
+        yield return new object[]
+        {
+            new CustomQueue<int>(new int[] { }),
+            new CustomQueue<int>(new int[] { })
+        };
+    }
+}
---
+++ Lab1/MyLibraryTest/ToArrayTest.cs
@@ -0,0 +1,24 @@
+﻿using MyLibrary;
+namespace MyLibraryTest;
+
+public class ToArrayTest
+{
+    [Theory]
+    [MemberData(nameof(ToArrayTestData))]
+    public void ToArray_FromIntQueue_SuccessfulConversion(int[] expectedArray)
+    {
+        CustomQueue<int> cq = new CustomQueue<int>(expectedArray);
+
+        var arr = cq.ToArray();
+        
+        Assert.Equal(expectedArray, arr);
+    }
+
+    public static IEnumerable<object[]> ToArrayTestData()
+    {
+        yield return new object[] { new int[] { 23, 53, 516, -1234, 1234, 51, 6, 73 } };
+        yield return new object[] { new int[] { 43, 72, 34, 43 } };
+        yield return new object[] { new int[] { -1 } };
+        yield return new object[] { new int[] { } };
+    }
+}
